7a6e08dff1b5b60803a785bf71984c4d6ae4a468
Denys Chornyi <denis.chornyi@gmail.com>
2026-09-21 19:48:30 +0200

    add debug message

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Hello from %s running in %s!", appName, environment)
+		log.Printf("Hello from %s running in %s!", appName, environment)
 		fmt.Fprintf(w, "Hello from %s running in %s!", appName, environment)
 	})
 
